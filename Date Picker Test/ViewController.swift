f588d7d48a46a8dd9b838fd50dbbbf080a64d9ac
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-04-12 15:37:40 +0900

    Change Color

--- Date Picker Test/ViewController.swift
+++ Date Picker Test/ViewController.swift
@@ -11,23 +11,40 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var myDatePicker: UIDatePicker!
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
+    
+    var myTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    @IBAction func changeDatePicker(_ sender: Any) {
-        print("change Date Picker")
-        print(myDatePicker.date)
-        
-        // dateFormat
         let formatter = DateFormatter()
-        formatter.dateFormat = "MM-dd HH:mm:ss EE"
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
         timeLabel.text = formatter.string(from: myDatePicker.date)
         
-//        timeLabel.text = String(describing: myDatePicker.date)
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in self.updateTime()
+        })
         
+    }
+    
+    func updateTime(){
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        currentTimeLabel.text = formatter.string(from: date)
+        
+        if currentTimeLabel.text == timeLabel.text {
+            view.backgroundColor = UIColor.red
+        }
+    }
+    @IBAction func stop(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+    }
+    
+    @IBAction func changeDatePicker(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        timeLabel.text = formatter.string(from: myDatePicker.date)
     }
 }
 
